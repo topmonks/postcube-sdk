946a105cd88e031bfb7c8f60908b30f963a9276e
Patrik Šimunič <patriksimunic@hotmail.com>
2022-01-17 08:13:41 +0100

    fix react

--- lib/react.ts
+++ lib/react.ts
@@ -4,54 +4,55 @@ import { PostCubeBLE, PostCube } from './apiBLE'
 export const usePostCubeBLE = (
     onCubeDiscovery?: (postCube: PostCube) => any,
 ): PostCubeBLE => {
-    const { useState, useEffect } = require('react')
+    // const { useState, useEffect } = require('react')
 
-    const [ toggle, setToggle ] = useState(false)
+    // const [ toggle, setToggle ] = useState(false)
 
-    const handlePostCubeBLEChange = () => {
-        setToggle(!toggle)
-    }
+    // const handlePostCubeBLEChange = () => {
+    //     setToggle(!toggle)
+    // }
 
-    useEffect(() => {
-        PostCubeBLE.onChange.listen(handlePostCubeBLEChange)
-        return () =>
-            PostCubeBLE.onChange.unlisten(handlePostCubeBLEChange)
-    }, [])
+    // useEffect(() => {
+    //     PostCubeBLE.onChange.listen(handlePostCubeBLEChange)
+    //     return () =>
+    //         PostCubeBLE.onChange.unlisten(handlePostCubeBLEChange)
+    // }, [])
 
-    useEffect(() => {
-        if (!onCubeDiscovery) {
-            return
-        }
+    // useEffect(() => {
+    //     if (!onCubeDiscovery) {
+    //         return
+    //     }
 
-        PostCubeBLE.onCubeDiscovered.listen(onCubeDiscovery)
-        return () =>
-            PostCubeBLE.onCubeDiscovered.unlisten(onCubeDiscovery)
-    }, [onCubeDiscovery])
+    //     PostCubeBLE.onCubeDiscovered.listen(onCubeDiscovery)
+    //     return () =>
+    //         PostCubeBLE.onCubeDiscovered.unlisten(onCubeDiscovery)
+    // }, [onCubeDiscovery])
 
     return PostCubeBLE
 }
 
 export const usePostCube = (postCube?: PostCube): PostCube => {
-    const { useState, useEffect } = require('react')
+    // const { useState, useEffect } = require('react')
 
-    const [ toggle, setToggle ] = useState(false)
-    const [ _postCube, setPostCube ] = useState(postCube)
+    // const [ toggle, setToggle ] = useState(false)
+    // const [ _postCube, setPostCube ] = useState(postCube)
 
-    const handlePostCubeChange = () => {
-        setToggle(!toggle)
-    }
+    // const handlePostCubeChange = () => {
+    //     setToggle(!toggle)
+    // }
 
-    useEffect(() => {
-        setPostCube(postCube)
+    // useEffect(() => {
+    //     setPostCube(postCube)
 
-        if (!postCube) {
-            return
-        }
+    //     if (!postCube) {
+    //         return
+    //     }
 
-        postCube.addListener('change', handlePostCubeChange)
-        return () =>
-            postCube.removeListener('change', handlePostCubeChange)
-    }, [postCube])
+    //     postCube.addListener('change', handlePostCubeChange)
+    //     return () =>
+    //         postCube.removeListener('change', handlePostCubeChange)
+    // }, [postCube])
 
-    return _postCube
+    // return _postCube
+    return postCube
 }
